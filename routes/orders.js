da9a563ccae482417e7ed81f17dcd3a9c89cef24
quocmv <quocmv8197@gmail.com>
2020-07-06 01:51:47 +0700

    protected route /orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { get } = require('mongoose')
 const router = express.Router()
 const orderController = require('../controller/ordercontroller')
 const ordercontroller = require('../controller/ordercontroller')
+
+const user = require('../permission/users')
 
 // -- GET ALL ORDER
 // -- POST SUBMIT NEW ORDER
@@ -12,9 +13,10 @@ router.route('/')
 
 // GET SPECIFICS ORDER
 router.route('/:id')
-    .get(orderController.getSpecificOrder)
-    .patch(orderController.updateSpecificOrder)
-    .delete(orderController.deleteSpecificOrder)
+    .get(user.userVerify, orderController.getSpecificOrder)
+    // give access for staff, manager, admin
+    .patch(user.userVerify, user.checkStaffRole, orderController.updateSpecificOrder)
+    .delete(user.userVerify, user.checkAdminRole,orderController.deleteSpecificOrder)
 
 // module export
 module.exports = router
