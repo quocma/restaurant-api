4da1a55d8ca6191e30f746f45fa2f0dba769183a
quocmv <quocmv8197@gmail.com>
2020-07-06 01:54:09 +0700

    protected route /booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,17 +1,18 @@
 const app = require('express')
 const router = app.Router()
 const bookingController = require('../controller/bookingcontroller')
+const user = require('../permission/users')
 
 
 router.route('/')
-    .get(bookingController.getBookingItem)
+    .get(user.userVerify, user.checkAdminRole, bookingController.getBookingItem)
     .post(bookingController.createOne)
    
 
 router.route('/:id') 
-    .get(bookingController.getBookingItem)
-    .patch(bookingController.update)
-    .delete(bookingController.delete)
+    .get(user.userVerify, user.checkStaffRole, bookingController.getBookingItem)
+    .patch(user.userVerify, user.checkStaffRole, bookingController.update)
+    .delete(user.userVerify, user.checkAdminRole, bookingController.delete)
 
 // module export
 module.exports = router;
